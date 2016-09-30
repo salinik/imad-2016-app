2d28f1ea129c457d897226e7e522bbaaf858de84
salinik <saluk12.123@gmail.com>
2016-09-30 08:36:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate(content));
+  res.send(createTemplate(articleone));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
