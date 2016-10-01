903e52e527fc6ad85376dcd80e4bd15064fb00e6
salinik <saluk12.123@gmail.com>
2016-10-01 10:49:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ var img=document.getElementById("madi");
 var marginLeft=0;
 function moveRight()
 {
-    marginRightt=marginRight+10;
-    img.style.marginRight=marginRight+`px`;
+    marginLeftt=marginLeft+10;
+    img.style.marginLeft=marginLeft+`px`;
 }
 img.onclick = function(){
     
