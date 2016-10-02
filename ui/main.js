469e9245dc53d60dcb0ccf6678f8c6f8478b45a0
salinik <saluk12.123@gmail.com>
2016-10-02 11:37:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
-//var element= document.getElementById(`main-text`);
-//element.innerHTML='new value';
+//couter code
+var counter=0;
+var button=document.getElementById("counter");
+button.onClick=function(){
+    counter=countr+1;
+    var span=document.getElementById("count");
+span.innerHTML=counter.toString();
+    
+};
