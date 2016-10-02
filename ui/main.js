2c5ca01df72ef26665d98e09ded20933fe7d1ad5
salinik <saluk12.123@gmail.com>
2016-10-02 11:42:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var counter=0;
 var button=document.getElementById("counter");
 button.onClick=function(){
-    counter=countr+1;
+    counter=counter+1;
     var span=document.getElementById("count");
 span.innerHTML=counter.toString();
     
