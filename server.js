91468957fea6cad87cc7bfbc6874fad5dbd3302a
salinik <saluk12.123@gmail.com>
2016-10-02 11:25:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
 });
  var counter=0;
 app.get('/counter', function(req,res){
-    counter=counte+1;
+    counter=counter+1;
     res.send(counter.toString());
     
 });
