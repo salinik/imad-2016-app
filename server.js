298ae1489349c46fdbe479f18b5ae86605f6d3fd
salinik <saluk12.123@gmail.com>
2016-10-10 12:29:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,8 +100,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/MyProfile', function (req, res) {
-   res.sendFile(path.join(__dirname,'ui',"MyProfile.html"));
+app.get('/ente', function (req, res) {
+   res.sendFile(path.join(__dirname,'ui',"ente.html"));
 });
 app.get('/ui/bi.css', function (req, res) {
    res.sendFile(path.join(__dirname,'ui',"bi.css"));
