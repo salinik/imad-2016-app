5d40d2667a56f216825f9ccf03c62e32e01022db
salinik <saluk12.123@gmail.com>
2016-11-24 20:49:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,15 +123,15 @@ app.get('/article-Three', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/mu.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'mu.jpg'));
+});
 
 app.get('/ui/head.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'head.png'));
 });
 app.get('/ui/prof.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'prof.png'));
-});
-app.get('/ui/mu.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'mu.jpg'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
